a40a06ce2990cfc6060f6607543a61303c4b1981
itsa-me-dea <wachadea@gmail.com>
2023-08-11 19:17:33 -0700

    Created Reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+const dayjs = require('dayjs')
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    // User who reacted
+    username: { 
+        type: String, 
+        required: true,
+      },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function() {
+        return dayjs(this.dateField).format('DD MMM YYYY HH:mm');
+      }, 
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
