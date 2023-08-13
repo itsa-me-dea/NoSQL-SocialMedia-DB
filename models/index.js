32f6881bc83cc7c1b11c95a736f87732507d9d79
itsa-me-dea <wachadea@gmail.com>
2023-08-12 21:27:08 -0700

    removed unnecessary files and code

--- models/index.js
+++ models/index.js
@@ -1,8 +1,5 @@
 const User = require('./User');
 const Thought = require('./Thought');
 const Reaction = require('./Reaction');
-// remove Tag and Application
-const Tag = require('./Tag');
-const Application = require('./Application');
 
-module.exports = { User, Thought, Reaction, Application, Tag };
+module.exports = { User, Thought, Reaction };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
-// delete appRoutes
-const appRoutes = require('./appRoutes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
-// delete appRoutes
-router.use('/apps', appRoutes);
 
 module.exports = router;
